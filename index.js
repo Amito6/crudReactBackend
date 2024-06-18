6c4e846fbdaabfd9f403185740c3deac921c4a2d
amito6 <amitchain24@gmail.com>
2024-06-18 00:56:19 -0400

    port-8080

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const dotenv = require("dotenv").config();
 const express = require("express");
 const server = express();
-server.listen(process.env.PORT || 5000);
+server.listen(8080);
 
 // enable body parser
 const bodyParser = require("body-parser");
